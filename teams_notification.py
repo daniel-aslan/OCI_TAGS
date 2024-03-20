57886a03ad56e923eefe01cd1ec844d0372927a1
daslan <daslan@fiu.edu>
2024-03-19 21:53:01 -0400

    Updated teams_notifications

--- teams_notification.py
+++ teams_notification.py
@@ -3,7 +3,7 @@
 import base64, pymsteams
 
 def teams_notifications(text_to_card):
-    encoded_teams_url = b'aHR0cHM6Ly9maXVkaXQud2ViaG9vay5vZmZpY2UuY29tL3dlYmhvb2tiMi9kODY2Y2M0YS1lOTFiLTRhOWItYTNkZi1jYmNlYzY3NWNlMGFAYWM3OWU1YTgtZTBlNC00MzRiLWEyOTItMmM4OWI1YzI4MzY2L0luY29taW5nV2ViaG9vay    9jOWU3MDJiZjZlMDc0OTFlOWNjMjRkMGRjM2UxYzhkYS81ZTliZDUyOS02YjYxLTQzYTItYmZkMy05MzJlMzRlNTQ2M2E='
+    encoded_teams_url = b'XXXXXXXXXXXXXXXXX'
     teams_url = (base64.b64decode(encoded_teams_url)).decode('ascii')
     card = pymsteams.connectorcard(teams_url)
     card.text(text_to_card)
